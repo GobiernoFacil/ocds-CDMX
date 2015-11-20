45c0757888315211461be5f8b7447e5d0a94ceed
hugovom <hugo.om@gmail.com>
2015-11-20 13:42:20 -0600

    add footer template

---
+++ templates/footer.php
@@ -0,0 +1,62 @@
+	<!--footer-->
+	<footer>
+		<div class="partners">
+			<div class="container">
+				<div class="row">
+					<h4>Con el apoyo de</h4>
+					<div class="col-sm-4">
+						<a href="http://www.bloombergassociates.org/" class="bloomberg">Bloomberg Associates</a>
+					</div>
+					<div class="col-sm-4">
+						<a href="http://www.open-contracting.org/" class="ocds">Open Contracting Data Standard</a>
+					</div>
+					<div class="col-sm-4">
+						<a href="http://imco.org.mx/" class="imco">IMCO</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-9">
+					<h2 class="cdmx_w">CDMX</h2>
+					<h3>CONTRATACIONES <strong>ABIERTAS</strong></h3>
+				</div>
+				<div class="col-sm-3 author">
+					<p>Forjado Artesanalmente por 
+						<a href="http://gobiernofacil.com/" class="gobiernofacil">Gobierno Fácil</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="copy">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-3">
+						<p>® 2015 Gobierno del Distrito Federal</p>
+					</div>
+					<div class="col-sm-4">
+						<p class="center">¿Dudas?  compras@df.gob.mx</p>
+					</div>
+					<div class="col-sm-5">
+						<p class="right">Plaza de la Constitución S/N Primer Piso, Centro, Cuauhtémoc, C.P. 06010</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-45473222-11', 'auto');
+  ga('send', 'pageview');
+
+	</script>
+	
+	<script data-main="js/main" src="/js/bower_components/requirejs/require.js"></script>
+	
+	
+</body>
+</html>
