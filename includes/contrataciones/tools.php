9850d125e04da121541735483602c6cdc7eac59c
hugovom <hugo.om@gmail.com>
2015-11-23 17:36:20 -0600

    update index

---
+++ includes/contrataciones/tools.php
@@ -0,0 +1,26 @@
+<div class="col-sm-8 col-sm-offset-2">
+	<h1>¿Qué más podrás hacer con  las <span>contrataciones abiertas</span>* de la CD<strong>MX</strong>?</h1>
+</div>
+<div class="col-sm-8 col-sm-offset-2 slide">
+	<div class="row">
+		<div class="col-sm-5">
+		<?php echo file_get_contents("img/dataviz.svg"); ?>	
+		</div>
+		<div class="col-sm-7">
+			<h2>Visualizar Datos</h2>
+			<p>Observa en qué productos o servicios gasta más el gobierno.</p>
+			<ul>
+				<li><a href="#" class="data current">Datos</a></li>
+				<li><a href="#" class="api">api</a></li>
+				<li><a href="#" class="sm">sm</a></li>
+			</ul>
+		</div>
+	</div>
+</div>	
+
+<div class="col-sm-12">
+	<a href="#" class="btn">Descubre como  usar los datos</a>
+</div>
+<div class="col-sm-4 col-sm-offset-8">
+	<p><small>* En la primera fase del proyecto el sitio solo contará con contrataciones de SEFIN, correspondientes a noviembre de 2015</small></p>
+</div>
--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	<section class="participacion">
 		<div class="container">
 			<div class="row">
-				<div class="col-sm-12">
+				<div class="col-sm-12 col-xs-10 col-sm-offset-0 col-xs-offset-1">
 					<h1><span>Tu participación</span> en las <span class="mx">contrataciones</span> de la 
 					CD<span class="mx">MX</span> <span>ayuda</span> a <span>combatir</span> <strong>la corrupción.</strong>
 					</h1>
@@ -59,32 +59,7 @@
 	<section class="tools">
 		<div class="container">
 			<div class="row">
-				<div class="col-sm-8 col-sm-offset-2">
-					<h1>¿Qué más podrás hacer con  las <span>contrataciones abiertas</span>* de la CD<strong>MX</strong>?</h1>
-				</div>
-				<div class="col-sm-8 col-sm-offset-2 slide">
-					<div class="row">
-						<div class="col-sm-5">
-						<?php echo file_get_contents("img/dataviz.svg"); ?>	
-						</div>
-						<div class="col-sm-7">
-							<h2>Visualizar Datos</h2>
-							<p>Observa en qué productos o servicios gasta más el gobierno.</p>
-							<ul>
-								<li><a href="#">Datos</a></li>
-								<li><a href="#">api</a></li>
-								<li><a href="#">sm</a></li>
-							</ul>
-						</div>
-					</div>
-				</div>	
-				
-				<div class="col-sm-12">
-					<a href="#" class="btn">Descubre como  usar los datos</a>
-				</div>
-				<div class="col-sm-4 col-sm-offset-8">
-					<p><small>* En la primera fase del proyecto el sitio solo contará con contrataciones de SEFIN, correspondientes a noviembre de 2015</small></p>
-				</div>
+				<?php include "includes/contrataciones/tools.php";?>
 			</div>
 		</div>
 	</section>
