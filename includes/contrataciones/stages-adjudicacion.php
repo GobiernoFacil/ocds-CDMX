11c60200e558d3896ad6c2b3386621cc6688613c
hugovom <hugo.om@gmail.com>
2015-11-25 13:44:26 -0600

    update stages

---
+++ includes/contrataciones/stages-adjudicacion.php
@@ -0,0 +1,16 @@
+<h2> <b></b> Adjudicación</h2>
+<!-- plenación-->
+<div class="planeacion">
+	<div class="description">
+		<p>Valor de la adjudicación</p>
+	</div>
+	<div class="description">
+		<p>Bienes y servicios otorgados</p>
+	</div>
+	<div class="main_pic">
+		<?php echo file_get_contents("img/svg/ilustracion_adjudicacion.svg"); ?>
+	</div>
+	<div class="description">
+		<p>Licitantes adjudicados</p>
+	</div>
+</div>
--- includes/contrataciones/stages-planeacion.php
+++ includes/contrataciones/stages-planeacion.php
@@ -1,14 +1,10 @@
-<div class="description">
-	<p>Datos presupuestales</p>
-</div>
-<div class="main_pic">
-	<?php echo file_get_contents("img/planeacion.svg"); ?>
-</div>
-
-
-<div class="description">
-	<p>Datos presupuestales</p>
-</div>
-<div class="main_pic">
-	<?php echo file_get_contents("img/svg/ilustracion_licitacion.svg"); ?>
+<h2> <b></b> Planeación</h2>
+<!-- plenación-->
+<div class="planeacion">
+	<div class="description">
+		<p>Datos presupuestales</p>
+	</div>
+	<div class="main_pic">
+		<?php echo file_get_contents("img/planeacion.svg"); ?>
+	</div>
 </div>
