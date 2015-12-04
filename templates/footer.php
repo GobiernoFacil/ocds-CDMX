b661d12df591396e6c841de98a39bda003cff916
hugovom <hugo.om@gmail.com>
2015-12-04 12:14:09 -0600

    add link to cdmx site

--- templates/footer.php
+++ templates/footer.php
@@ -19,7 +19,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-9">
-					<h2 class="cdmx_w">CDMX</h2>
+					<h2><a href="http://www.df.gob.mx/" class="cdmx_w">CDMX</a></h2>
 					<h3>CONTRATACIONES <strong>ABIERTAS</strong></h3>
 				</div>
 				<div class="col-sm-3 author">
--- templates/header.php
+++ templates/header.php
@@ -44,7 +44,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-xs-3 col-sm-3">
-				<h2 class="cdmx">CDMX | SEFIN</h2>
+				<h2><a href="http://www.df.gob.mx/"  class="cdmx">CDMX | SEFIN</a></h2>
 				</div>
 				<div class="col-xs-6 col-sm-6">
 					<h1><a href="index.php">CONTRATACIONES <strong>ABIERTAS</strong></a></h1>
