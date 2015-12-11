b9ca3a48293fa5a16e4ff3e16d36efa14a48c6dc
hugovom <hugo.om@gmail.com>
2015-12-11 10:03:57 -0600

    remove old files

--- includes/contrataciones/tools_nav.php
+++
@@ -1,5 +0,0 @@
-<ul>
-	<li><a href="#" id="tools-visualize-btn" class="data current">Datos</a></li>
-	<li><a href="#" id="tools-api-btn" class="api">api</a></li>
-	<li><a href="#" id="tools-social-btn" class="sm">sm</a></li>
-</ul>
