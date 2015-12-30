4cb25af50bee0f9821750c84479f363e6d93722d
Boris Cuapio Morales <boris@iMac.lan>
2015-12-29 19:54:21 -0600

    the data framework

---
+++ provider.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+
+<h1>Contrataciones abiertas</h1>
+<script data-main="/js/apps/provider/main" src="js/bower_components/requirejs/require.js"></script>
+</body>
+</html>
