97c2c3e42195732965ce739236c40c46b9a6511d
elcoruco <el.coruco@gmail.com>
2015-12-31 01:03:34 -0600

    backbone for the landing-contract

--- contrato-landing.php
+++ contrato-landing.php
@@ -6,6 +6,13 @@
 <body>
 
 <h1>Contrataciones abiertas</h1>
+<form>
+  <p>
+    <input type="text" name="query" id="general-search">
+  </p>
+</form>
+
+<nav><a href="#">&lt; Dependencia</a></nav>
 <script data-main="/js/apps/provider/main" src="js/bower_components/requirejs/require.js"></script>
 </body>
 </html>
