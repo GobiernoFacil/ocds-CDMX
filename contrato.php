7f338e1240c13bfc1d7f84387a75c736e65318ca
hugovom <hugo.om@gmail.com>
2016-01-11 19:05:27 -0600

    add contract template

---
+++ contrato.php
@@ -0,0 +1,16 @@
+<?php 	
+	$body_class 	= "contract";
+	$title 			= "Contrato";
+	$description 	= "Contrato";
+	$og_image		= "img/og/contrato-cdmx.png";
+	$canonical		= $url;
+	include "templates/header.php";?>
+
+<div class="col-sm-3">
+	<h2>Hola</h2>
+</div>	
+	
+	
+	
+	
+<?php include "templates/footer.php";?>
