75121f994bd248e2a8c3230b5b8f21452b8afa4a
hugovom <hugo.om@gmail.com>
2016-01-11 20:46:02 -0600

    update contract view #2

--- contrato.php
+++ contrato.php
@@ -5,12 +5,78 @@
 	$og_image		= "img/og/contrato-cdmx.png";
 	$canonical		= $url;
 	include "templates/header.php";?>
-
-<div class="col-sm-3">
-	<h2>Hola</h2>
-</div>	
-	
-	
+<div class="breadcrumb">
+	<div class="container">
+	</div>
+</div>
+<article>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-3 sidebar">
+				<div class="header">
+					<h3>LICITACIÓN</h3>
+					<h2>ADQUISICIÓN DE MATERIALES PARA OFICINA 003</h2>
+				</div>
+			</div>	
+			<div class="col-sm-9 info">
+				<div class="row divider">
+					<div class="col-sm-12">
+						<p class="title_section">Contrato</p>
+						<h1>PAPELERIA PARA EL ALMACÈN CENTRAL. </h1>
+						<h2>CS-019/2015 <span class="label active">ACTIVO</span></h2>
+					</div>
+				</div>
+				<div class="row divider">
+					<div class="col-sm-6">
+						<h2 class="amount"><span>$</span> 23,304.40 <span>MXN</span></h2>
+					</div>
+					<div class="col-sm-6">
+						<p class="title_section">PROVEEDOR</p>
+						<p>ABASTECEDORA ARAGONESA SA DE CV</p>
+					</div> 
+				</div>
+				<div class="row divider">
+					<div class="col-sm-5">
+						<p class="title_section">COMPRADOR</p>
+						<p>SECRETARÍA DE FINANZAS </p>
+					</div>
+					<div class="col-sm-3">
+						<p class="title_section">FECHA</p>
+						<p>31/01/2015</p>
+					</div>
+					<div class="col-sm-4">
+						<p class="title_section">PERÍODO</p>
+					</div> 
+				</div>
+				
+				<div class="row">
+					<div class="col-sm-8">
+						<p class="title_section">ARTÍCULOS</p>
+						<ul>
+							<li class="row">
+								<span class="col-sm-9"> CARPETA C/PERCALINA T/OFICIO (VA.COLORES)</span>
+								<span class="col-sm-3">400 PZA</span>
+							</li>
+							<li class="row">
+								<span class="col-sm-9">BROCHE TRIUNFO No. 2 (MARIPOSA)</span>
+								<span class="col-sm-3">1500 CS</span>
+							</li>
+							<li class="row">
+								<span class="col-sm-9">BLOCK NOTAS AUTOADHERIBLES DE 3 X 3</span>
+								<span class="col-sm-3">600 PZA</span>
+							</li>
+							
+						</ul>
+					</div>
+					
+					<div class="col-sm-4">
+						<p class="title_section">DOCUMENTOS</p>
+					</div> 
+				</div>
+			</div>
+		</div>
+	</div>
+</article>
 	
 	
 <?php include "templates/footer.php";?>
