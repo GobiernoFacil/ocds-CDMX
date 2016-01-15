f36ca7c1b7940e0623abb0daab762fe769833d9e
hugovom <hugo.om@gmail.com>
2016-01-14 19:11:52 -0600

    update home v2

--- home-v2.php
+++ home-v2.php
@@ -8,10 +8,15 @@
 	
 <div class="breadcrumb">
 	<div class="container">
-		<nav>
-  <a href="#" id="dependencia-a">Dependencia A</a>
-  <a href="#" id="dependencia-a">Dependencia B</a>
-</nav>
+		<nav class="row">
+			<div class="col-sm-5">
+			Contratos
+			</div>
+			<div class="col-sm-7 right">
+				<a href="#" id="dependencia-a" class="dependencia live">DA</a>
+				<a href="#" id="dependencia-a" class="dependencia">DB</a>
+			</div>
+		</nav>
 	</div>
 </div>
 
@@ -19,6 +24,17 @@
 
 
 <div class="viz_home">
+	<header>
+		<div class="col-sm-4">
+			<p><span>DEPENDENCIA</span>SEFIN</p>
+		</div>
+		<div class="col-sm-4 center">
+			<p><span>CONTRATOS POR DEPENDENCIA</span> <strong>10</strong></p>
+		</div>
+		<div class="col-sm-4">
+			<p><span>TOTAL (MXN)</span>$<strong>267</strong> millones </p>
+		</div>
+	</header>
 	<p id="publisher-name"></p>
 	<p id="contratos-total-num"></p>
 	<p id="contratos-total-money"></p>
@@ -31,6 +47,31 @@
 		<div class="col-sm-12">
 			<h3>Contratos</h3>
 			<ul class="list">
+				<li class="row">
+					<div class="col-sm-9 top">
+						<h2><a href="contrato.php">PAPELERIA PARA EL ALMACÈN CENTRAL. <span>CS-019/2015</span></a></h2>
+					</div>
+					<div class="col-sm-3 amount top">
+						<p><span>$</span> 46608.8 <span>MXN</span></p>
+					</div>
+					<div class="clearfix"></div>
+					<div class="icons">
+						<div class="col-sm-4">
+							<p>Comprador: <a href="#">SEFIN</a></p>
+						</div>
+						<div class="col-sm-4">
+							<p>Proveedor: <a href="#">CLAUDIA GABRIELA IÑIGUEZ OCHOA</a></p>
+						</div>
+						<div class="col-sm-4">
+							<ul>
+								<li>Activo</li>
+								<li>3</li>
+								<li>10 meses</li>
+							</ul>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+				</li>
 				<li class="row">
 					<div class="col-sm-9 top">
 						<h2><a href="contrato.php">PAPELERIA PARA EL ALMACÈN CENTRAL. <span>CS-019/2015</span></a></h2>
