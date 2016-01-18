0efce6dfa5134b0faf9e5b42dd3f6a1a609284b0
hugovom <hugo.om@gmail.com>
2016-01-18 13:57:42 -0600

    update dependencia view

--- dependencia.php
+++ dependencia.php
@@ -1,57 +1,141 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title></title>
-</head>
-<body>
+	<?php 	
+	$body_class 	= "dependencia";
+	$title 			= "Contrataciones de la Secretaría de Finanzas de la CDMX";
+	$description 	= "Contratos de SEFIN";
+	$og_image		= "img/og/sefin-contrataciones-abiertas-cdmx.png";
+	$canonical		= $url;
+	include "templates/header.php";?>
 
-<h1>Contrataciones abiertas</h1>
-<form>
-  <p>
-    <input type="text" name="query" id="general-search">
-  </p>
-</form>
+<div class="container">
+	<div class="row list">
+		<div class="col-sm-12">
+			<div class="box">
+		
+				<p>Dependencia</p>
+				<h1 id="publisher-name"><a href="http://www.finanzas.df.gob.mx/">Secretaría de Finanzas</a></h1>
+				<div class="divider"></div>
+				<p id="releases-buyer-name"></p>
+				<p>
+				  <span id="address-streetAddress">Dr. Lavista 144</span>
+				  <span id="address-locality">Cuauhtémoc</span>
+				  <span id="address-region">DF</span>
+				  CP.<span id="address-postalCode">06720</span>
+				</p>
+				
+				<div class="divider"></div>
+				
+				<div class="row">
+					<div class="col-sm-3">
+						<p>CONTRATACIONES TOTAL(MXN)</p>
+						<h2 id="contrataciones-total-money"><span>$</span>0 <span>MILLONES</span></h2>
+					</div>
+					<div class="col-sm-3">
+						<p>LICITACIONES</p>
+						<h2 id="licitaciones-total-num">11</h2>
+					</div>
+					<div class="col-sm-3">
+						<p>ADJUDICACIONES</p>
+						<h2 id="adjudicaciones-total-num">0</h2>
+					</div>
+					<div class="col-sm-3">
+						<p>PROMEDIO POR LICITACIÓN (MXN)</p>
+						<h2 id="gasto-promedio-money"><span>$</span>0 <span>MILLONES</span></h2>
+					</div>
+					
+				</div>
+				<div class="divider"></div>
+				<div id="linemap">
+				  <h3>Total por contrato</h3>
+				  <p>información recopilada desde <span id="tremmap-data-from"></span></p>
+				</div>		
 
-<h2>Dependencia</h2>
-<p id="publisher-name"></p>
-<p id="releases-buyer-name"></p>
-<p>
-  <span id="address-streetAddress"></span>
-  <span id="address-locality"></span>
-  <span id="address-region"></span>
-  CP.<span id="address-postalCode"></span>
-</p>
+			</div>
 
-<p id="contrataciones-total-money"></p>
-<p id="licitaciones-total-num"></p>
-<p id="adjudicaciones-total-num"></p>
-<p id="gasto-promedio-money"></p>
-
-<div id="linemap">
-  <h3>Total por contrato</h3>
-  <p>información recopilada desde <span id="tremmap-data-from"></span></p>
+		</div>
+		
+		<div class="col-sm-6">
+			<div class="box">
+				<div class="row">
+					<div class="col-sm-8">
+						<h3>Proveedores más beneficiados</h3>
+					</div>
+					<div class="col-sm-4">
+						<p class="right">TOTAL (MXN)</p>
+					</div>
+				</div>
+				<ul id="lucky-providers">
+					<li class="row">
+						<span class="col-sm-8">
+							<a href="#">ABASTECEDORA ARAGONESA SA DE CV</a>
+						</span>
+						<span class="col-sm-4 right">
+							$4,123,084
+						</span>
+					</li>
+					
+				</ul>
+			</div>
+		</div>
+		
+		<div class="col-sm-6">
+			<div class="box">
+				<div class="row">
+					<div class="col-sm-8">
+						<h3>Licitaciones más costosas</h3>
+					</div>
+					<div class="col-sm-4">
+						<p class="right">TOTAL (MXN)</p>
+					</div>
+				</div>
+				<ul id="licitaciones-costosas">
+					<li class="row">
+						<span class="col-sm-8">
+							<a href="#">PAPELERIA PARA EL ALMACÉN CENTRAL</a>
+						</span>
+						<span class="col-sm-4 right">
+							$4,123,084
+						</span>
+					</li>
+				</ul>
+			</div>
+		</div>
+		
+		<div class="col-sm-12">
+			<div class="box">
+				<div class="row">
+					<div class="col-sm-6">
+						<h3>Todas las licitaciones: <strong id="award-counter-filter">1</strong></h3>
+					</div>
+					<div class="col-sm-6">
+						<form>
+				  <p>
+				    <select name="filter" id="award-filter-select">
+				      <option>Todas</option>
+				      <option>A</option>
+				      <option>B</option>
+				    </select>
+				  </p>
+				</form>
+					</div>
+				</div>
+				<ul id="awards-by-filter">
+					<li class="row">
+						<span class="col-sm-6">
+							<a href="#">PAPELERIA PARA EL ALMACÉN CENTRAL</a>
+						</span>
+						<span class="col-sm-3">
+							01-01-2015
+						</span>
+						<span class="col-sm-3 right">
+							$4,123,084
+						</span>
+					</li>
+				</ul>
+			</div>
+		</div>
+		
+	</div>
 </div>
 
-<h3>Proveedores más beneficiados</h3>
-<ul id="lucky-providers"></ul>
-
-<h3>Licitaciones más costosas</h3>
-<ul id="licitaciones-costosas"></ul>
-
-<p>Todas las licitaciones: <span id="award-counter-filter"></span></p>
-<form>
-  <p>
-    <select name="filter" id="award-filter-select">
-      <option>Todas</option>
-      <option>A</option>
-      <option>B</option>
-    </select>
-  </p>
-</form>
-<ul id="awards-by-filter">
-  
-</ul>
-
 <script data-main="/js/apps/provider/main" src="js/bower_components/requirejs/require.js"></script>
-</body>
-</html>
+<?php include "templates/footer.php";?>
