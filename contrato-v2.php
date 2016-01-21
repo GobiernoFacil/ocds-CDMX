b62361f72aca1f61eef065e05c8848f8f266101e
hugovom <hugo.om@gmail.com>
2016-01-21 17:16:46 -0600

    update contract & awards data

--- contrato-v2.php
+++ contrato-v2.php
@@ -28,45 +28,57 @@
 	$og_image		= "img/og/contrato-cdmx.png";
 	$canonical		= $url;
 	include "templates/header.php";?>
+<!-->
+<pre>
+	<?php //var_dump($elcontrato->releases[0]->contracts);?>
+</pre>
+<!-->
 <div class="breadcrumb">
 	<div class="container">
 		<a href="#"><strong>&lt;</strong> SEFIN</a>
 	</div>
 </div>
 <article>
-			<div class="col-sm-3 sidebar">
-				<div class="header">
-					<?php if ($elcontrato->releases[0]->tender):?>
-					<h3>LICITACIÓN</h3>
-					<h2><?php echo $elcontrato->releases[0]->tender->title;?></h2>
-					<?php else:?>
-					<h3>PLANEACIÓN</h3>
-					<h2><?php echo $elcontrato->releases[0]->planning->budget->project;?></h2>
-					<?php endif;?>
-				</div>
-				<nav>
-					<ul class="timeline">
-						<li><a href="#"  id="btn-contract-nav" class="nav_stage current" data-title="Contratación">
-							<?php echo file_get_contents("img/nav_contratacion.svg"); ?>	</a>
-							<ul id="nav_contract">
-								<li class="active"><a id="btn-contract-1" href="#" data-title="Hace 5 meses" class="t_right"></a></li>
-								<li><a id="btn-contract-2" href="#" data-title="Hace 5 meses" class="t_left"></a></li>
-							</ul>
-						</li>
-						<li><a href="#" id="btn-award-nav" class="nav_stage" data-title="Adjudicación"><?php echo file_get_contents("img/nav_adjudicacion.svg"); ?></a> 
-							<ul id="nav_award">
-								<li><a id="btn-award-1" href="#" data-title="Hace 6 meses" class="t_right"></a></li>
-								<li><a id="btn-award-2" href="#" data-title="Hace 6 meses" class="t_left"></a></li>
-							</ul>
-						</li>
-						<li><a href="#" id="btn-tender" class="nav_stage" data-title="Licitación"><?php echo file_get_contents("img/nav_licitacion.svg"); ?></a></li>
-						<li><a href="#" id="btn-planning"class="nav_stage" data-title="Planeación"><?php echo file_get_contents("img/nav_planeacion.svg"); ?></a></li>
+	<div class="col-sm-3 sidebar">
+		<div class="header">
+			<?php if ($elcontrato->releases[0]->tender):?>
+			<h3>LICITACIÓN</h3>
+			<h2><?php echo $elcontrato->releases[0]->tender->title;?></h2>
+			<?php else:?>
+			<h3>PLANEACIÓN</h3>
+			<h2><?php echo $elcontrato->releases[0]->planning->budget->project;?></h2>
+			<?php endif;?>
+		</div>
+		<nav>
+			<ul class="timeline">
+				<?php if($elcontrato->releases[0]->contracts):?>
+				<li><a href="#"  id="btn-contract-nav" class="nav_stage current" data-title="Contratación">
+					<?php echo file_get_contents("img/nav_contratacion.svg"); ?>	</a>
+					<ul id="nav_contract">
+						<li class="active"><a id="btn-contract-1" href="#" data-title="Hace 5 meses" class="t_right"></a></li>
+						<li><a id="btn-contract-2" href="#" data-title="Hace 5 meses" class="t_left"></a></li>
 					</ul>
-				</nav>
-			</div>	
+				</li>
+				<?php endif;?>
+				<?php if($elcontrato->releases[0]->awards):?>
+				<li><a href="#" id="btn-award-nav" class="nav_stage" data-title="Adjudicación"><?php echo file_get_contents("img/nav_adjudicacion.svg"); ?></a> 
+					<ul id="nav_award">
+						<li><a id="btn-award-1" href="#" data-title="Hace 6 meses" class="t_right"></a></li>
+						<li><a id="btn-award-2" href="#" data-title="Hace 6 meses" class="t_left"></a></li>
+					</ul>
+				</li>
+				<?php endif;?>
+				<?php if($elcontrato->releases[0]->planning):?>
+				<li><a href="#" id="btn-tender" class="nav_stage" data-title="Licitación"><?php echo file_get_contents("img/nav_licitacion.svg"); ?></a></li>
+				<?php endif;?>
+				<?php if($elcontrato->releases[0]->tender):?>
+				<li><a href="#" id="btn-planning"class="nav_stage" data-title="Planeación"><?php echo file_get_contents("img/nav_planeacion.svg"); ?></a></li>
+				<?php endif;?>
+			</ul>
+		</nav>
+	</div>	
 			
-			<div class="col-sm-9 info">
-				
+	<div class="col-sm-9 info">			
 				<!-- tender-->
 				<div id="tender">
 					<div class="row divider">
@@ -213,215 +225,134 @@
 							</ol>
 						</div> 
 					</div>
-					
 				</div>
 				
 				<!--awards-->
-				<div id="award-1">
-					<div class="row divider">
-						<div class="col-sm-12">
-							<p class="title_section">Adjudicación</p>
-							<h1>PRUEBA 2 CALIDAD</h1>
-							<h2>ID: 1 <span class="label active">ACTIVO</span></h2>
-						</div>
-					</div>
-					
-					<div class="row divider">
-						<div class="col-sm-6">
-							<p class="title_section">Descripción</p>
-							<p>PAPELERIA SEFIN 2</p>
-						</div>
-						<div class="col-sm-6">
-							<h2 class="amount"><span>$</span> 23,304.40 <span>MXN</span></h2>
-						</div>
-					</div>
-					
-					<div class="row divider">
-						<div class="col-sm-6">
-							<p class="title_section">Proveedor</p>
-							<p><a href="#">ABASTECEDORA ARAGONESA SA DE CV</a></p>
-						</div>
-						<div class="col-sm-6">
-							<p class="title_section">Fecha</p>
-							<p>30-01-2015</p>
-						</div>
-					</div>
-					
-					<div class="row">
-						<div class="col-sm-8">
-							<p class="title_section">ARTÍCULOS</p>
-							<ul>
-								<li class="row">
-									<span class="col-sm-9"> CARPETA C/PERCALINA T/OFICIO (VA.COLORES)</span>
-									<span class="col-sm-3">400 PZA</span>
-								</li>
-								<li class="row">
-									<span class="col-sm-9">BROCHE TRIUNFO No. 2 (MARIPOSA)</span>
-									<span class="col-sm-3">1500 CS</span>
-								</li>								
-							</ul>
-						</div>
-						
-						<div class="col-sm-4">
-							<p class="title_section">DOCUMENTOS</p>
-						</div>
-					</div>
-				</div>
-			 
+				<?php if($elcontrato->releases[0]->awards):?>
 				
-				<div id="award-2">
-					<div class="row divider">
-						<div class="col-sm-12">
-							<p class="title_section">Adjudicación</p>
-							<h1>PRUEBA 3 CALIDAD</h1>
-							<h2>ID: 2 <span class="label active">ACTIVO</span></h2>
-						</div>
-					</div>
-					
-					<div class="row divider">
-						<div class="col-sm-6">
-							<p class="title_section">Descripción</p>
-							<p>PAPELERIA SEFIN 2</p>
-						</div>
-						<div class="col-sm-6">
-							<h2 class="amount"><span>$</span> 13,356.90 <span>MXN</span></h2>
-						</div>
-					</div>
-					
-					<div class="row divider">
-						<div class="col-sm-6">
-							<p class="title_section">Proveedor</p>
-							<p><a href="#">CLAUDIA GABRIELA IÑIGUEZ OCHOA</a></p>
-						</div>
-						<div class="col-sm-6">
-							<p class="title_section">Fecha</p>
-							<p>30-01-2015</p>
-						</div>
-					</div>
-					
-					<div class="row">
-						<div class="col-sm-8">
-							<p class="title_section">ARTÍCULOS</p>
-							<ul>
-								<li class="row">
-									<span class="col-sm-9">BLOCK NOTAS AUTOADHERIBLES DE 3 X 3</span>
-									<span class="col-sm-3">600 PZA</span>
-								</li>								
-							</ul>
+					<?php foreach($elcontrato->releases[0]->awards as $award):?>
+					<div id="award-<?php echo $award->id;?>">
+						<div class="row divider">
+							<div class="col-sm-12">
+								<p class="title_section">Adjudicación</p>
+								<h1><?php echo $award->title;?></h1>
+								<h2>ID: <?php echo $award->id;?> <span class="label <?php echo $award->status;?>"><?php echo $award->status == "active" ? "ACTIVO":"";?></span></h2>
+							</div>
 						</div>
 						
-						<div class="col-sm-4">
-							<p class="title_section">DOCUMENTOS</p>
+						<div class="row divider">
+							<div class="col-sm-6">
+								<p class="title_section">Descripción</p>
+								<p><?php echo $award->description;?></p>
+							</div>
+							<div class="col-sm-6">
+								<h2 class="amount"><span>$</span> <?php echo number_format($award->value->amount,2,'.',',');?> <span><?php echo $award->value->currency;?></span></h2>
+							</div>
+						</div>
+						
+						<div class="row divider">
+							<div class="col-sm-6">
+								<p class="title_section">Proveedor</p>
+								<p><a href="#"><?php echo $award->suppliers[0]->name;?></a></p>
+							</div>
+							<div class="col-sm-6">
+								<p class="title_section">Fecha</p>
+								<?php $time_award = strtotime($award->date);?>
+								<p><?php echo date('d-m-Y',$time_award);?></p>
+							</div>
+						</div>
+						
+						<div class="row">
+							<div class="col-sm-8">
+								<p class="title_section">ARTÍCULOS</p>
+								<ul>
+									<?php foreach ($award->items as $item):?>
+									<li class="row">
+										<span class="col-sm-9"><?php echo $item->description;?></span>
+										<span class="col-sm-3"><?php echo $item->quantity;?> <?php echo $item->unit->name;?></span>
+									</li>
+									<?php endforeach;?>							
+								</ul>
+							</div>
+							
+							<div class="col-sm-4">
+								<p class="title_section">DOCUMENTOS</p>
+							</div>
 						</div>
 					</div>
-				</div>
+					<?php endforeach;?>
+				
+				<?php endif;?>
 				
 				<!-- contratos-->
-				<div id="contract-1">
-				<div class="row divider">
-					<div class="col-sm-12">
-						<p class="title_section">Contrato</p>
-						<h1>PAPELERIA PARA EL ALMACÈN CENTRAL. </h1>
-						<h2>CS-019/2015 <span class="label active">ACTIVO</span></h2>
-					</div>
-				</div>
-				<div class="row divider">
-					<div class="col-sm-6">
-						<h2 class="amount"><span>$</span> 23,304.40 <span>MXN</span></h2>
-					</div>
-					<div class="col-sm-6">
-						<p class="title_section">PROVEEDOR</p>
-						<p>ABASTECEDORA ARAGONESA SA DE CV</p>
-					</div> 
-				</div>
-				<div class="row divider">
-					<div class="col-sm-5">
-						<p class="title_section">COMPRADOR</p>
-						<p>SECRETARÍA DE FINANZAS </p>
-					</div>
-					<div class="col-sm-3">
-						<p class="title_section">FECHA</p>
-						<p>31/01/2015</p>
-					</div>
-					<div class="col-sm-4">
-						<p class="title_section">PERÍODO</p>
-					</div> 
-				</div>
-				
-					<div class="row">
-						<div class="col-sm-8">
-							<p class="title_section">ARTÍCULOS</p>
-							<ul>
-								<li class="row">
-									<span class="col-sm-9"> CARPETA C/PERCALINA T/OFICIO (VA.COLORES)</span>
-									<span class="col-sm-3">400 PZA</span>
-								</li>
-								<li class="row">
-									<span class="col-sm-9">BROCHE TRIUNFO No. 2 (MARIPOSA)</span>
-									<span class="col-sm-3">1500 CS</span>
-								</li>
-								<li class="row">
-									<span class="col-sm-9">BLOCK NOTAS AUTOADHERIBLES DE 3 X 3</span>
-									<span class="col-sm-3">600 PZA</span>
-								</li>
-								
-							</ul>
+				<?php if($elcontrato->releases[0]->contracts):?>
+					<?php foreach($elcontrato->releases[0]->contracts as $contract):?>
+					<div id="contract-<?php echo $contract->id;?>">
+					<div class="row divider">
+						<div class="col-sm-12">
+							<p class="title_section">Contrato</p>
+							<h1><?php echo $contract->description;?></h1>
+							<h2><?php echo $contract->title;?> <span class="label <?php echo $contract->status;?>">
+							<?php echo $contract->status == "active" ? "ACTIVO" : "";?></span></h2>
 						</div>
-						
-						<div class="col-sm-4">
-							<p class="title_section">DOCUMENTOS</p>
+					</div>
+					<div class="row divider">
+						<div class="col-sm-6">
+							<p class="title_section">MONTO</p>
+							<h2 class="amount"><span>$</span><?php echo number_format($contract->value->amount,2,'.',',');?><span><?php echo $contract->value->currency;?></span></h2>
+						</div>
+						<div class="col-sm-6">
+							<p class="title_section">PROVEEDOR</p>
+							<?php foreach($elcontrato->releases[0]->awards as $award):?>
+								<?php if ($contract->awardID == $award->id):?>
+									<p><a href=""><?php echo $award->suppliers[0]->name;?></a></p>
+								<?php endif;?>
+							<?php endforeach;?>
 						</div> 
 					</div>
-				</div>
-				
-				<div id="contract-2">
-				<div class="row divider">
-					<div class="col-sm-12">
-						<p class="title_section">Contrato</p>
-						<h1>PAPELERIA PARA EL ALMACÈN CENTRAL. </h1>
-						<h2>CS-018/2015 <span class="label active">ACTIVO</span></h2>
-					</div>
-				</div>
-				<div class="row divider">
-					<div class="col-sm-6">
-						<h2 class="amount"><span>$</span>13,356.90 <span>MXN</span></h2>
-					</div>
-					<div class="col-sm-6">
-						<p class="title_section">PROVEEDOR</p>
-						<p>CLAUDIA GABRIELA IÑIGUEZ OCHOA</p>
-					</div> 
-				</div>
-				<div class="row divider">
-					<div class="col-sm-5">
-						<p class="title_section">COMPRADOR</p>
-						<p>SECRETARÍA DE FINANZAS </p>
-					</div>
-					<div class="col-sm-3">
-						<p class="title_section">FECHA</p>
-						<p>31/01/2015</p>
-					</div>
-					<div class="col-sm-4">
-						<p class="title_section">PERÍODO</p>
-					</div> 
-				</div>
-				
-					<div class="row">
-						<div class="col-sm-8">
-							<p class="title_section">ARTÍCULOS</p>
-							<ul>
-								<li class="row">
-									<span class="col-sm-9">BLOCK NOTAS AUTOADHERIBLES DE 3 X 3</span>
-									<span class="col-sm-3">600 PZA</span>
-								</li>
-								
-							</ul>
+					<div class="row divider">
+						<div class="col-sm-5">
+							<p class="title_section">COMPRADOR</p>
+							<p><a href="/dependencia.php"><?php echo $elcontrato->releases[0]->tender->procuringEntity->name;?></a></p>
 						</div>
-						
+						<div class="col-sm-3">
+							<p class="title_section">FECHA DE FIRMA</p>
+							<?php $time_contract = strtotime($contract->dateSigned);?>
+							<p><?php echo date('d/m/Y',$time_contract);?></p>
+						</div>
 						<div class="col-sm-4">
-							<p class="title_section">DOCUMENTOS</p>
+							<p class="title_section">PERÍODO</p>
+							<?php 
+								$period_startDate = strtotime($contract->period->startDate);
+								$period_endDate = strtotime($contract->period->endDate);
+							?>
+							<p><strong><?php echo date('d/m/Y',$period_startDate);?></strong> a <strong><?php echo date('d/m/Y',$period_endDate);?></strong></p>
+							
 						</div> 
 					</div>
-				</div>
+					
+						<div class="row">
+							<div class="col-sm-8">
+								<p class="title_section">ARTÍCULOS</p>
+								<ul>
+									<?php foreach($contract->items as $item):?>
+									<li class="row">
+										<span class="col-sm-9"><?php echo $item->description;?></span>
+										<span class="col-sm-3 right"><?php echo number_format($item->quantity);?> <?php echo $item->unit->name;?></span>
+									</li>
+									<?php endforeach;?>									
+								</ul>
+							</div>
+							
+							<div class="col-sm-4">
+								<p class="title_section">DOCUMENTOS</p>
+							</div> 
+						</div>
+					</div>
+					<?php endforeach;?>
+
+				<?php endif;?>
+				
 				
 				<!-- planning-->
 				<div id="planning">
