223a8c4ae5ee57111044301dd9dba3977996fade
hugovom <hugo.om@gmail.com>
2016-03-18 13:18:10 -0600

    fix typo

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-12 col-xs-10 col-sm-offset-0 col-xs-offset-1">
-					<h1>Estamos construyento una herramienta con la que podrás <span>dar seguimiento</span> a las <span class="mx">contrataciones</span> de la 
+					<h1>Estamos construyendo una herramienta con la que podrás <span>dar seguimiento</span> a las <span class="mx">contrataciones</span> de la 
 					CD<span class="mx">MX</span>.
 					</h1>
 					<a href="#" class="btn cta">Vuelve pronto</a>
